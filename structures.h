41fe665b61ecf1872418edc12c36fd2e76461005
Leo <leohernandesvasc@gmail.com>
2021-09-28 10:01:41 -0300

    Estrutura do nodo com campo de "extensão" + comentários

--- structures.h
+++ structures.h
@@ -1,10 +1,13 @@
+/* Lista simplesmente encadeada para armazenar todos os filhos (pastas e arquivos) de um nodo */
 typedef struct TipoListaFilhos{
     struct TipoDiretorio* filho;
     struct TipoListaFilhos* prox;
 }ListaFilhos;
 
-typedef struct TipoDiretorio{
+/* Árvore armazenando distribuição de arquivos e pastas num cluster */
+typedef struct TipoCluster{
     char* nome;
+    char* extensao;
     struct TipoDiretorio* pai;
     struct TipoListaFilhos* filhos;
-}NodoDiretorio;
+}NodoCluster;
