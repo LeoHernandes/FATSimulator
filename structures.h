77e35611194546dc1169f51bbacd12b495548d6a
Leo <leohernandesvasc@gmail.com>
2021-09-28 09:50:31 -0300

    estruturas feitas

--- structures.h
+++ structures.h
@@ -1,3 +1,10 @@
-typedef struct{
-    char* ponteiro;
-}
+typedef struct TipoListaFilhos{
+    struct TipoDiretorio* filho;
+    struct TipoListaFilhos* prox;
+}ListaFilhos;
+
+typedef struct TipoDiretorio{
+    char* nome;
+    struct TipoDiretorio* pai;
+    struct TipoListaFilhos* filhos;
+}NodoDiretorio;
