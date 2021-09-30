7099aae95959e44b2d5253ebe9eb7fa90482b55e
Bruno Aires <brunoferreiraaires@gmail.com>
2021-09-30 16:45:41 -0300

    Atualização
    Alteração para trabalhar com um arquivo binário. Além de padronizar como os dados são armazenados no arquivo.

--- funcoes.c
+++ funcoes.c
@@ -11,8 +11,10 @@ MetaDados metaDados = {256, 32000, 0, 1};
 //ponteiro para o arquivo
 FILE *arq;
     int bytesCluster = 0;
+    char zero = 0;
+    char valor255 = 255;
     NodoCluster root = {"root", ".TXT", 0, NULL};
-    arq = fopen("ArqDisco.txt", "r+b");
+    arq = fopen("ArqDisco.bin", "r+b");
     if (arq == NULL){
         printf("Problemas na criacao do arquivo\n");
         return;
@@ -21,22 +23,22 @@ FILE *arq;
         fwrite("\n", sizeof(char), 1, arq);
 
         for(int j = 0; j < 256; j++){
-            fwrite("00000000", 8, 1, arq);
+            fwrite(&zero, sizeof(char), 1, arq);
         }
         fwrite("\n", sizeof(char), 1, arq);
         //La�o que a cria��o dos 256 clusters
         for(int m = 0; m < 255; m++){
             //La�o que controla a cria��o de um cluster com 32KB, todos com 0
             for(int i = 0; i < 32000; i++){
-            fputs("0", arq);
+            fwrite(&zero, sizeof(char), 1, arq);
             }
             fwrite("\n", sizeof(char), 1, arq);
         }
         fseek(arq, sizeof(MetaDados), SEEK_SET);
         fwrite("\n", sizeof(char), 1, arq);
-        fwrite("FFFFFFFF", 8, 1, arq);
+        fwrite(&valor255, sizeof(char), 1, arq);
         //Cria��o do cluster root
-        fseek(arq, sizeof(MetaDados)+2049, SEEK_SET);
+        fseek(arq, sizeof(MetaDados)+257, SEEK_SET);
         fwrite("\n", sizeof(char), 1, arq);
         fwrite(&root, sizeof(NodoCluster), 1, arq);
 
@@ -46,7 +48,7 @@ FILE *arq;
 
 MetaDados pegaMetadados(){
     FILE *arq;
-    arq = fopen("ArqDisco.txt", "rb+");
+    arq = fopen("ArqDisco.bin", "rb+");
     MetaDados metaDados;
     if (arq == NULL){ // Se houve erro na abertura{
      printf("Problemas na abertura do arquivo\n");
@@ -83,22 +85,22 @@ void detectaComando(char comando[], int diretorioAtual, char tabela[]){
     }else if(strstr(comando, "Sair") != NULL){
 
     }else{
-        printf("Comando n�o reconhecido.\n");
+        printf("Comando nao reconhecido.\n");
     }
 
 }
 
 void pegaTabela(char tabela[]){
     FILE *arq;
-    arq = fopen("ArqDisco.txt", "r");
+    arq = fopen("ArqDisco.bin", "r");
     if (arq == NULL){ // Se houve erro na abertura{
      printf("Problemas na abertura do arquivo\n");
      return;
      }else{
     fseek(arq, sizeof(MetaDados)+1, SEEK_SET);
     fwrite("\n", sizeof(char), 1, arq);
-    for(int i = 0; i < 2048; i++){
-        fscanf(arq, "%c", &tabela[i]);
+    for(int i = 0; i < 256; i++){
+        fread(&tabela[i], sizeof(char),1, arq);
     }
     }
     fclose(arq);
@@ -108,13 +110,9 @@ void pegaTabela(char tabela[]){
 
 int primeiraPosicaoDisponivel(char tabela[]){
     int controle = 0;
-    for(int i = 0; i < 2048; i += 8){
-        for(int m = 0; m < 8; m++){
-            if(tabela[m+i] != '0'){
-                m = 8;
-            }else if(m == 7){
-                return i;
-            }
+    for(int i = 0; i < 256; i++){
+        if(tabela[i] == 0){
+            return i;
         }
     }
     return 0;
